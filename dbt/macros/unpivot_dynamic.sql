9f952ee3e8bf3c0c3d19573852e4e030cd92eec9
j0rdan <jas4235@outlook.com>
2023-09-07 17:56:38 -0400

    'migrating...'

---
+++ dbt/macros/unpivot_dynamic.sql
@@ -0,0 +1,56 @@
+/* 
+
+    Macro:  unpivot_big_table(table_name_ref, num_fixed_columns)
+
+    Description:    This macro dynamically generates SQL for a redshift UNPIVOT command. 
+                    Especially useful for cases where you need to reshape a table from wide to long, 
+                    and you have a VERY wide table (like AP exam results' 89 columns).  Redshift's 
+                    UNPIVOT command requires you to list out all the columns by name, and this can be 
+                    tedious.  This macro essentially just lists out all the column names as strings 
+                    dynamically for you.
+
+                    A previous version of this operation used Jinja to reshape the table with nested 
+                    loops to create short little tables into a giant UNION operation. Here we use 
+                    redshift's UNPIVOT with the assumption that it will be more performant and easier 
+                    to read.
+
+    Returns:    an SQL SELECT statement to UNPIVOT data from a specified table with a specified number 
+                of fixed columns. The result of running the SQL will turn all columns from the UNPIVOT 
+                into pairs of 'orig_col_name','orig_value'
+
+    param:  table_name_ref - a string of the table to be referenced, the code uses dbt's: ref(tablename)
+    param:  num_fixed_columns - an integer of the n left-most columns in the table you want to stay fixed 
+            for data to unpivot around. The remaining columns will all be turned into key-value pairs with 
+            column names 'orig_col_name' and 'orig_value'
+*/
+
+
+
+{% macro unpivot_big_table(table_name_ref, num_fixed_columns) %}
+
+    -- Get a list of column names from the table, and split into two lists of (1) the
+    {% set my_table_columns = adapter.get_columns_in_relation(ref(table_name_ref)) %}
+
+    -- for the fixed columns, construct a string of "comma", "separated", "lists","of", "double-quoted","col", "names"
+    {% set fixed_cols_str = (
+        '"' ~ my_table_columns[:num_fixed_columns]
+        | map(attribute="name")
+        | join('", "') ~ '"'
+    ) %}
+
+    -- for the set of column to unpivot, construct a string of "comma", "separated", "lists", "of", "double-quoted","col", "names"
+    {% set pivot_cols_str = (
+        '"' ~ my_table_columns[num_fixed_columns:]
+        | map(attribute="name")
+        | join('", "') ~ '"'
+    ) %}
+
+    -- Construct the SELECT statament for the UNPIVOT
+    select {{ fixed_cols_str }}, orig_col_name, orig_value
+    from
+        (
+            select {{ fixed_cols_str }}, {{ pivot_cols_str }} from {{ ref(table_name_ref) }}
+        ) as sourcetable
+        unpivot (orig_value for orig_col_name in ({{ pivot_cols_str }}))
+
+{% endmacro %}
--- dbt/models/staging/dashboard_pii/stg_dashboard_pii__pd_international_opt_ins.sql
+++ dbt/models/staging/dashboard_pii/stg_dashboard_pii__pd_international_opt_ins.sql
@@ -2,38 +2,7 @@ with
 international_opt_ins as (
     select * 
     from {{ ref('base_dashboard_pii__pd_international_opt_ins')}}
-),
-
-final as (
-    select 
-        international_opt_in_id,
-        user_id                                             as teacher_user_id,
-        created_at                                          as form_submitted_at,
-        updated_at                                          as updated_at,
-
-        json_extract_path(form_data, 'firstName')           as first_name,
-        json_extract_path(form_data, 'firstNamePreferred')  as pref_name,
-        json_extract_path(form_data, 'lastName')            as last_name,
-        json_extract_path(form_data, 'email')               as email,
-        json_extract_path(form_data, 'emailAlternate')      as email_alt,
-        json_extract_path(form_data, 'gender')              as gender,
-        json_extract_path(form_data, 'schoolDepartment')    as school_department,
-        json_extract_path(form_data, 'schoolMunicipality')  as school_municipality,
-        json_extract_path(form_data, 'schoolName')          as school_name,
-        json_extract_path(form_data, 'schoolCity')          as school_city,
-        json_extract_path(form_data, 'schoolCountry')       as school_country,
-        json_extract_path(form_data, 'ages')                as age_taught,
-        json_extract_path(form_data, 'subjects')            as subject_taught,
-        json_extract_path(form_data, 'resources')           as cs_rescources,
-        json_extract_path(form_data, 'robotics')            as robotics_rescources,
-        json_extract_path(form_data, 'date')                as workshop_date,
-        json_extract_path(form_data, 'workshopOrganizer')   as workshop_organizer,
-        json_extract_path(form_data, 'workshopFacilitator') as workshop_facilitator,
-        json_extract_path(form_data, 'workshopCourse')      as workshop_course,
-        json_extract_path(form_data, 'emailOptIn')          as email_opt_in,
-        form_data
-    from international_opt_ins 
 )
 
 select * 
-from final 
+from international_opt_ins 
