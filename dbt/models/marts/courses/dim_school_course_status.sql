9f4d2dba209dc6da72fc851c4e701a0df1286643
jørdan <jas4235@outlook.com>
2023-09-20 22:17:29 -0400

    taking a break from this madness

---
+++ dbt/models/marts/courses/dim_school_course_status.sql
@@ -0,0 +1,82 @@
+-- Model: dim_school_course_status
+-- Scope: course status data collected for active courses only collected by school
+-- Author: js
+
+with 
+teachers as (
+    select school_id,
+        school_year,
+        course_name,
+        null as num_students_started,
+        count(distinct case when started_at is not null then teacher_user_id end) as num_teachers_started,
+        min(started_at) as first_started_at,
+        dense_rank() over(partition by school_id, course_name order by school_year asc) as sequence_num,
+        dense_rank() over(partition by school_id, course_name order by school_year desc) as sequence_num_inv
+    from {{ ref('dim_teachers') }}
+    where started_at is not null 
+    {{ dbt_utils.dbt_utils.group_by(3)}}
+),
+
+students as (
+    select school_id,
+        school_year,
+        course_name,
+        count(distinct case when started_at is not null then teacher_user_id end) as num_users_started,
+    from {{ ref('dim_students') }}
+    where started_at is not null 
+    {{ dbt_utils.dbt_utils.group_by(3)}}
+),
+
+combined as (
+    select 
+        teachers.school_id,
+        teachers.school_year,
+        teachers.course_name, -- really need to update this to course_id for FK joins
+        students.num_students_started,
+        teachers.num_teachers_started,
+        teachers.first_started_at,
+        teachers.seq
+    from teachers 
+    left join students 
+        on teachers.school_id = students.school_id 
+        and teachers.school_year = students.school_year 
+        and teachers.course_name = students.course_name
+),
+
+final as (
+    select school_id,
+        school_year,
+        course_name,
+
+        -- course_status: this logic needs heavy review @bakerfranke @allison-code-dot-org 
+        case when seq = 1 then 'active new' 
+             when seq > 1 then 'active retained'
+             when seq is null 
+              and lag(seq,1) over(partition by school_id, course_name order by school_year) is not null 
+                then 'inactive this year'
+            when seq is null
+             and sum(seq) over (partition by school_id, course_name order by school_year rows unbounded preceding) is not null 
+             and lag(seq) over (partition by school_id, course_name order by school_year) is not null 
+                then 'inactive churn'
+            when seq > 1 
+             and lag(seq,1) over (partition by school_id, course_name order by school_year) is null 
+                then 'active reacquired'
+            else 'market'
+        end as course_status, 
+        
+        -- (js) flag based on presence of course activity
+        case when seq is not null then 1 else 0 end as is_active,
+
+        -- (js) aggregating here to ensure our grain
+        sum(num_students_started) as total_students_started,
+        sum(num_teachers_started) as total_teachers_started,
+        {# total_teachers_trained,
+        total_teachers_trained_this_year, #}
+
+        max(seq) as total_years_active
+    from combined
+    {{ dbt_utils.group_by('5') }}
+)
+
+select * 
+from final 
--- dbt/models/marts/schools/fct_school_stats.sql
+++ dbt/models/marts/schools/fct_school_stats.sql
@@ -1,8 +1,12 @@
 with 
-
 schools as (
     select * 
     from {{ ref('stg_dashboard__schools') }}
+),
+
+school_districts as (
+    select * 
+    from {{ ref('stg_dashboard__school_districts')}}
 ),
 
 school_stats_by_years as (
@@ -16,24 +20,20 @@ school_stats_19_20 as (
     where school_year = '2019-2020'
 ),
 
-frl_eligibility_status as (         -- (js) this model should probably be migrated to dim_schools
+frl_eligibility_status as (         
+    -- (js) this model should probably be migrated to dim_schools
     -- status for SY 20-21
     select ssby.school_id, 
-    case 
-        when coalesce(ssby2.total_frl_eligible,ssby.total_frl_eligible) is not null 
-        then 1 else 0 end                                           as is_frl_eligible,
-        coalesce(ssby2.total_frl_eligible,ssby.total_frl_eligible)  as total_frl_eligible,
-
+        case 
+            when coalesce(ssby2.total_frl_eligible,ssby.total_frl_eligible) is not null 
+            then 1 else 0 end                                           as is_frl_eligible,
+            coalesce(ssby2.total_frl_eligible,ssby.total_frl_eligible)  as total_frl_eligible,
     from school_stats_by_years as ssby
     inner join school_stats_19_20 as ssby2
         on ssby.school_id = ssby2.school_id 
     left join survey_years as sy 
         on ssby.school_id = sy.school_id
     where survey_year.school_year = '2020-2021'
-),
-school_districts as (
-    select * 
-    from {{ ref('stg_dashboard__school_districts')}}
 ),
 
 survey_years as (
@@ -45,10 +45,13 @@ survey_years as (
     group by school_id
 ),
 
--- (js) still missing here: teacher_trainings data 
+{# 
+-- (js) still missing here: teacher_trainings data  
+#}
 
 aggregated as (
-    select school_id,
+    select 
+        school_id,
         coalesce(total_frl_eligible,0) / coalesce(total_students) as pct_frl_eligible,
         case when pct_frl_eligible > 0.5 then 1 else 0 end as is_high_needs        
     from eligibility_status
@@ -59,5 +62,14 @@ combined as (
         school_id, 
         
     from schools -- (js) eventually dim_schools
-
+    left join school_info 
+        on schools.school_info_id = school_info.school_info_id
+    left join school_districts 
+        on schools.school_district_id = school_districts.school_district_id
+    left join survey_years 
+        on schools.school_id = survey_years.school_id 
+    left join school_stats_by_years as ssby 
+        on schools.school_id = ssby.school_id 
+    left join frl_eligibility_status as frles 
+        on schools.school_id = frles.school_id 
 )
