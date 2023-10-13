c79cb0ef41d1421f671a21dcd0133a0d6cbfc15c
allison@code.org <allison@code.org>
2023-10-13 23:53:23 +0000

    combined dim_schools and fct_school_stats, can't figure out dim_csf_attendance but committing what I have

--- dbt/models/marts/schools/dim_schools.sql
+++ dbt/models/marts/schools/dim_schools.sql
@@ -20,6 +20,9 @@ combined as (
     select
         -- schools
         schools.school_id,
+        schools.city,
+        schools.state,
+        schools.zip,
         school_stats_by_years.school_year,
         school_stats_by_years.is_stage_el,
         school_stats_by_years.is_stage_mi,
@@ -33,13 +36,56 @@ combined as (
         case when total_frl_eligible_students / total_students::float > 0.5
             then 1 else 0 
         end as is_high_needs,
+        schools.last_known_school_year_open,
 
         --school_districts
         school_districts.school_district_id,
         school_districts.school_district_name,
 
+        -- nces school metrics
+        school_stats_by_years.total_students,
+        school_stats_by_years.count_student_am,
+        school_stats_by_years.count_student_as,
+        school_stats_by_years.count_student_hi,
+        school_stats_by_years.count_student_bl,
+        school_stats_by_years.count_student_wh,
+        school_stats_by_years.count_student_hp,
+        school_stats_by_years.count_student_tr,
+        school_stats_by_years.total_frl_eligible_students,
+
+        nullif(
+            sum(school_stats_by_years.count_student_am 
+                + school_stats_by_years.count_student_hi
+                + school_stats_by_years.count_student_bl
+                + school_stats_by_years.count_student_hp)
+            ,0) as total_urg_no_tr_students,
+
+        nullif(
+            sum(school_stats_by_years.count_student_am
+                + school_stats_by_years.count_student_hi
+                + school_stats_by_years.count_student_bl
+                + school_stats_by_years.count_student_hp
+                + school_stats_by_years.count_student_tr)
+            ,0) as total_urg_students,
+
+        nullif(
+            sum(school_stats_by_years.count_student_am  
+                + school_stats_by_years.count_student_as  
+                + school_stats_by_years.count_student_hi  
+                + school_stats_by_years.count_student_bl  
+                + school_stats_by_years.count_student_wh  
+                + school_stats_by_years.count_student_hp
+                + school_stats_by_years.count_student_tr)
+            ,0) as total_students_calculated,
+        
+        -- calculations 
+        total_urg_students / total_students_calculated::float as urg_percent,
+
+        total_urg_no_tr_students / total_students_calculated::float as urg_no_tr_percent,
+        
+        total_frl_eligible_students / total_students::float as frl_eligible_percent,
+        
         -- dates 
-        schools.last_known_school_year_open,
         min(schools.created_at) as school_created_at,
         max(schools.updated_at) as school_last_updated_at
     from schools
@@ -48,7 +94,7 @@ combined as (
         and school_stats_by_years.row_num = 1
     left join school_districts
         on schools.school_district_id = school_districts.school_district_id
-    {{ dbt_utils.group_by(15) }}
+    {{ dbt_utils.group_by(27) }}
 )
 
 select *
--- dbt/models/marts/schools/nces_school_stats_by_years.sql
+++ dbt/models/marts/schools/nces_school_stats_by_years.sql
@@ -1,17 +1,7 @@
 with 
 school_stats_by_years as (
     select 
-        school_id,
-        school_year,
-        total_students,
-        count_student_am,
-        count_student_as,
-        count_student_hi,
-        count_student_bl,
-        count_student_wh,
-        count_student_hp,
-        count_student_tr,
-        total_frl_eligible_students,
+        *,
 
         -- pre-process these totals here
         nullif(
@@ -39,25 +29,12 @@ school_stats_by_years as (
                 + count_student_tr)
             ,0) as total_students_calculated
     from {{ ref('stg_dashboard__school_stats_by_years') }}
-    {{ dbt_utils.group_by(11) }}
+    {{ dbt_utils.group_by(40) }}
 ),
 
 combined as (
     select 
-        school_id,
-        school_year,
-        total_students,
-        count_student_am,
-        count_student_as,
-        count_student_hi,
-        count_student_bl,
-        count_student_wh,
-        count_student_hp,
-        count_student_tr,
-        total_frl_eligible_students,
-        total_urg_no_tr_students,
-        total_urg_students,
-        total_students_calculated,
+        *,
 
         -- calculations 
         total_urg_students / total_students_calculated::float as urg_percent,
@@ -74,22 +51,5 @@ combined as (
 )
 
 select 
-    school_id,
-    school_year,
-    total_students,
-    count_student_am,
-    count_student_as,
-    count_student_hi,
-    count_student_bl,
-    count_student_wh,
-    count_student_hp,
-    count_student_tr,
-    total_frl_eligible_students,
-    total_urg_no_tr_students,
-    total_urg_students,
-    total_students_calculated,
-    urg_percent,
-    urg_no_tr_percent,
-    -- cast(urg_percent_true as decimal(3,2)) as urg_percent_true,              -- (ag) check validity of this with business cases
-    frl_eligible_percent
+    *
 from combined 
