b8c77e472b1fc8683ebca3e46c13c568d2b9baa3
jørdan <jordan.springer@code.org>
2023-10-23 16:51:31 +0000

    saving

---
+++ dbt/models/intermediate/int_section_status.sql
@@ -0,0 +1,16 @@
+/* 
+1. Design:
+    section_id int,
+    school_year varchar(10),
+    is_active bit
+
+2. Definitions:
+    is_active:  5+ students completing 
+                at least 1 level
+3. Sources:
+    dim_user_levels
+    int_student_teacher_section_complete
+
+Ref: dataops-316
+*/
+
