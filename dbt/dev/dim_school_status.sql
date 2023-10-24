91cdf7131d41c0f0d25502384a2c30f2763989cc
jørdan <jas4235@outlook.com>
2023-10-24 15:47:52 -0400

    moar

--- dbt/dev/dim_school_status.sql
+++ dbt/dev/dim_school_status.sql
@@ -2,6 +2,27 @@
 -- Scope: course status data collected for active courses only collected by school
 -- Note: follow school_course_status; remove lead/lag() component
 -- Author: js
+with 
+teachers as (
+    select 
+        school_id,
+        school_year,
+        teacher_id
+    from {{ ref('dim_teachers') }}
+    where is_international = 0 
+),
+
+sections as (
+    select * 
+    from {{ ref('dim_sections') }}
+    where user_id in (select teacher_id from teachers)
+),
+
+students as (
+    select * 
+    from {{ ref('dim_students') }}
+    where is_international = 0 
+),
 
 -- end product:
 final as (
@@ -14,15 +35,26 @@ final as (
         case when seq = 1 then 'active new' 
              when seq > 1 then 'active retained'
              when seq is null 
-              and lag(seq,1) over(partition by school_id, course_name order by school_year) is not null 
+              and lag(seq,1) over(
+                    partition by school_id, course_name 
+                    order by school_year) is not null 
                 then 'inactive this year'
+
             when seq is null
-             and sum(seq) over (partition by school_id, course_name order by school_year rows unbounded preceding) is not null 
-             and lag(seq) over (partition by school_id, course_name order by school_year) is not null 
+             and sum(seq) over (
+                    partition by school_id, course_name 
+                    order by school_year rows unbounded preceding) is not null 
+             and lag(seq) over (
+                    partition by school_id, course_name 
+                    order by school_year) is not null 
                 then 'inactive churn'
+
             when seq > 1 
-             and lag(seq,1) over (partition by school_id, course_name order by school_year) is null 
+             and lag(seq,1) over(
+                    partition by school_id, course_name 
+                    order by school_year) is null 
                 then 'active reacquired'
+
             else 'market'
         end as course_status, 
         
