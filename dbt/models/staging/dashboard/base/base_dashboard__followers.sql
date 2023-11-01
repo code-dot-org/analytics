2792a480307472bcd7f4cebd8816e02453d78af0
allison@code.org <allison@code.org>
2023-11-01 19:49:54 +0000

    a few small fixes to make tests run

--- dbt/models/staging/dashboard/base/base_dashboard__followers.sql
+++ dbt/models/staging/dashboard/base/base_dashboard__followers.sql
@@ -6,7 +6,7 @@ source as (
 renamed as (
     select
         id as follower_id,
-        student_user_id,
+        student_user_id as student_id,
         section_id,
         created_at,
         updated_at
