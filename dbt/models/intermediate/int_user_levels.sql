6e9fee63ffe3982b541eba92ac40b8864243aece
jørdan <jas4235@outlook.com>
2023-12-01 13:08:27 -0500

    saving progress

---
+++ dbt/models/intermediate/int_user_levels.sql
@@ -0,0 +1,28 @@
+-- int_user_levels
+
+with 
+user_levels as (
+    select * 
+    from {{ ref('stg_dashboard__user_levels') }}
+),
+
+students as (
+    select * 
+    from {{ ref('dim_students') }}
+    where student_id in (select user_id from user_levels)
+),
+
+final as (
+    select 
+        user_levels.user_id,
+        students.is_international,
+        user_levels.level_id,
+        user_levels.level_source_id,
+        user_levels.script_id
+    from user_levels 
+    join students 
+        on user_levels.user_id = students.student_id 
+)
+
+select * 
+from final
