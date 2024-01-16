799f13dd36e65acc97b1c0f7d801734cde6f4709
Baker Franke <baker@code.org>
2024-01-16 00:03:12 +0000

    submitting changes to fct_school_acquisition -- see int_school_weeks

--- dbt/models/intermediate/int_school_weeks.sql
+++ dbt/models/intermediate/int_school_weeks.sql
@@ -1,4 +1,41 @@
--- This JINJA worked I think but was really slow.  Defered to composing in CTEs below with unions, joins, and multiplication
+{# NOTES:
+
+This creates a corallary to int_school_years, but for weeks of the school year in SCHOOL YEAR ORDER.
+
+We frequently need to Report metrics on a weekly basis based on timestamps.  We want to do this using
+ISO weeks to the greatest extent possible, however, because our school year is defined to start on July 1 and end June 30.
+It means that the start of the school year can start either in ISO week 26 or 27 depending on the year.
+Furthermore, some years I have 52 weeks and some bleed into a 53rd week.
+When reporting you're over a year metrics based on school year, we want to line up the beginning of the year with July 1
+But adhere to standard iso week, boundaries as much as possible, since it's likely that other metrics might be grouped the same way.
+
+The SOLUTION. 
+
+1. This table numbers the "school year weeks" with week 1 being possibly a fragment (less than 7 days) of a week that starts with July 1,
+and stops at the ISO week boundary that first comes after july 1.
+
+2. Weeks 2 through 51 fall on standard I so weak boundaries.
+
+3. Week 52 (or 53 depending on the year) is another possible fragment of a week running from the ISO week boundary that is closest to June 30,
+and running up through June 30.
+
+As a result, for example, the end of one school (june 30) and the start of another (july 1) year may fall entirely within ISO week 27, but in
+this table that might be split between "school year week" 52 (being 4 days) and school year week 1 of the next year being 3 days.
+
+The final table includes started_at and ended_at fields for joining to (a la our traditional practice with int_school_years)
+And for each school year week it also includes an ISO week reference.  Again, ISO weeks match up 1:1 with school_year weeks (albeit different values)
+EXCEPT for the week 52/week 1 boundaries.  
+
+METHOD:
+1. Generate all possible dates (literally all the days) and their iso weeks from the beginning of code.org time (~July 1, 2013)
+2. Flag all the weeks where either the ISO week changes between days OR the school year changes
+3. The "school year week" is the sum/count of all the flags up to that date WITHIN (partitioned by) that school year. 
+
+#}
+
+
+
+-- This JINJA worked to generate all the days I think but was really slow.  Defered to composing in CTEs below with unions, joins, and multiplication
 -- {% set n = 15000 %} -- Set your desired range here
 -- with numbers_large as (
 -- {% for i in range(n) %}
@@ -7,6 +44,9 @@
 -- {% endfor %}
 -- )
 
+
+-- Next 3 CTEs are SQL-y way of generating all the days (dates) from the beginning of code.org
+-- time, culminating in the CTE 'date_range'
 with numbers_small as (
     select 0 as num
     union all
@@ -59,7 +99,7 @@ flagged_week_changes as (
         dr.*,
         lag(iso_week) over (order by date) as prev_iso_week,
 
-        -- 'flag' dates where an iso week changes, or the school year (july 1) changes
+        -- 'flag' dates where an iso week changes, or the school year changes (july 1)
         case
             when
                 iso_week != prev_iso_week
