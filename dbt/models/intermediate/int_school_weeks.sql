eb46044c674c20edf1874f043d1903c7cb165f82
Baker Franke <baker@code.org>
2024-01-17 20:09:35 +0000

    attempt 2 to commit - fixed bug that caused a full day gap between ended_at and next started_at

--- dbt/models/intermediate/int_school_weeks.sql
+++ dbt/models/intermediate/int_school_weeks.sql
@@ -127,7 +127,7 @@ select
     iso_week,
     school_year_week,
     min(date)::timestamp as started_at,     --0:0:0 on start date
-    dateadd(s, -1, max(date)) as ended_at,  --23:59:59 on end date
+    dateadd(s, -1, max(date)+1) as ended_at,  --subtract 1 second from the next day to give 23:59:59 on end date
     (ended_at::date - started_at::date) + 1 as days_interval
 from school_week_calc
 group by
