ae3aee75556d5ce4f39edd17b6af9cf2af0ab1d8
Natalia Zacipa Mejia <natalia@code.org>
2024-01-31 22:02:00 +0000

    Changes to account for all type of users being included: 1. Allowed all courses 2. Added user_type (joined to stg_users)

--- dbt/models/marts/students/dim_student_courses.sql
+++ dbt/models/marts/students/dim_student_courses.sql
@@ -10,13 +10,14 @@ Updated 12/6/23:
     last_activity_at timestamp
 
 2. Definitions:
-    - a student is included in this table if they attempted a level of a given course within a given SY
+    - a user is included in this table if they attempted a level of a given course within a given SY
     - this table is used to determine "active" students within a section and assigning a "section_status"
 
 3. Sources:
-    dim_user_levels
+    stg_dashboard__user_levels
     course_structure
     school_years
+    stg_dashboard__users
 
 Ref: dataops-316
 */
@@ -29,6 +30,7 @@ user_levels as (
         script_id,
         created_at
     from {{ ref('stg_dashboard__user_levels') }}
+    where attempts > 0
 ),
 
 course_structure as (
@@ -44,27 +46,25 @@ school_years as (
     from {{ ref('int_school_years') }}
 ),
 
+users as (
+    select *
+    from {{ ref('stg_dashboard__users') }}
+),
+
 combined as (
     select 
-        ul.user_id                              as student_id,
+        ul.user_id,
+        u.user_type
         sy.school_year,
-        case 
-            when cs.course_name_true in (  
-                'ai',
-                'csf',
-                'csp',
-                'csd',
-                'hoc',
-                'csc',
-                'csa'
-            ) then cs.course_name_true
-        end                                     as course_name,  
+        cs.course_name_true,  
         min(ul.created_at)                      as first_activity_at,
 		max(ul.created_at)                      as last_activity_at
 	from user_levels ul 
 	join course_structure cs
 		on ul.script_id = cs.script_id 
         and ul.level_id = cs.level_id 
+    join users u
+        on ul.user_id = u.user_id
 	join school_years sy 
 		on ul.created_at 
             between sy.started_at and sy.ended_at
