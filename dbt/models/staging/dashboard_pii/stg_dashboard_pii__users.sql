73a36b11e5999c3bc15107487ef9132d7829a17a
baker@code.org <baker@code.org>
2024-01-31 20:20:50 +0000

    commit but still testing

--- dbt/models/staging/dashboard_pii/stg_dashboard_pii__users.sql
+++ dbt/models/staging/dashboard_pii/stg_dashboard_pii__users.sql
@@ -30,6 +30,39 @@ renamed as (
 
         --PII
         races,
+         case
+            -- If races contains 'hispanic', return 'hispanic'
+            when races like '%hispanic%' then 'hispanic'
+
+            -- If races matches any of these specific strings, return 'no_response'
+            when
+                races in ('closed_dialog', 'nonsense', 'opt_out')
+                then 'no_response'
+
+            -- If races contains a comma AND is_urg return 'two or more urg'
+            when races like '%,%' and is_urg = 1 then 'two_or_more_urg'
+
+            -- If races contains a comma and not caught by case above, then urg is 0 or null, return 'two or more non urg'
+            when races like '%,%' then 'two_or_more_non_urg'
+
+            -- If races is NULL, return NULL
+            when races is null then 'not_collected'
+
+            -- Default case: return the input value
+            else races -- Additional logic may be required here
+        end as race_group,
+
+        nullif(lower(gender), '') as gender,
+        case
+            when gender = 'm' then 'm'
+            when gender = 'f' then 'f'
+            when gender = 'n' then 'nb' ------  non-binary
+            when gender = 'o' then 'nl' ------  not listed
+            ----  empty string or '-'
+            when gender = '' or gender = '-' then 'no_response'
+            when gender is null or gender in ('k','d','x','v','b','a','A') then 'not_collected'
+            else 'unexpected value: ' || gender
+        end as gender_group,
 
         -- user info
         is_urg,
@@ -52,41 +85,13 @@ renamed as (
         -- dates         
         case when user_type = 'student' then user_id end as student_id,
         case when user_type = 'teacher' then user_id end as teacher_id,
-        datediff(year, birthday, current_date) as age_years,
-        nullif(lower(gender), '') as gender,
-        case
-            when gender = 'm' then 'm'
-            when gender = 'f' then 'f'
-            when gender = 'n' then 'nb' ------  non-binary
-            when gender = 'o' then 'nl' ------  not listed
-            ----  empty string or '-'
-            when gender = '' or gender = '-' then 'no_response'
-            when gender is null then 'not_collected'
-            else 'unexpected value: ' || gender
-        end as gender_group,
-        case
-            -- If races contains 'hispanic', return 'hispanic'
-            when races like '%hispanic%' then 'hispanic'
+        datediff(year, birthday, current_date) as age_years
 
-            -- If races matches any of these specific strings, return 'no_response'
-            when
-                races in ('closed_dialog', 'nonsense', 'opt_out')
-                then 'no_response'
-
-            -- If races contains a comma AND is_urg return 'two or more urg'
-            when races like '%,%' and is_urg = 1 then 'two_or_more_urg'
-
-            -- If races contains a comma and not caught by case above, then urg is 0 or null, return 'two or more non urg'
-            when races like '%,%' then 'two_or_more_non_urg'
-
-            -- If races is NULL, return NULL
-            when races is null then 'not_collected'
-
-            -- Default case: return the input value
-            else races -- Additional logic may be required here
-        end as race_group
+       
     from users
 )
 
 select *
 from renamed
+
+SELECT count(*), race_group FROM {{ref('stg_dashboard_pii__users')}} group by 2 order by 2 LIMIT 100
