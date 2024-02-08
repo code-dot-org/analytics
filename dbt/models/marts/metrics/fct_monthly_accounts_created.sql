8bc3d1eab9ef85930bfee04312e95300db088dc1
baker@code.org <baker@code.org>
2024-02-08 16:57:38 +0000

    added country to monthly signins metrics AND converted to dim_users

--- dbt/models/marts/metrics/fct_monthly_accounts_created.sql
+++ dbt/models/marts/metrics/fct_monthly_accounts_created.sql
@@ -5,38 +5,41 @@ day, but past months should remain unchanged.
 #}
 
 with 
-users as (
-    select 
-        user_id,
-        user_type,
-        created_at,
-        current_sign_in_at
-    from {{ref('stg_dashboard__users')}}
-    where current_sign_in_at is not null 
-),
+-- users as (
+--     select 
+--         user_id,
+--         user_type,
+--         created_at,
+--         current_sign_in_at
+--     from {{ref('stg_dashboard__users')}}
+--     where current_sign_in_at is not null 
+-- ),
 
-user_geos as (
-    select 
-        user_id,
-        is_international,
-        country
-    from {{ref('stg_dashboard__user_geos')}}
-    where user_id in (select user_id from users)
-),
+-- user_geos as (
+--     select 
+--         user_id,
+--         is_international,
+--         country
+--     from {{ref('stg_dashboard__user_geos')}}
+--     where user_id in (select user_id from users)
+-- ),
 
-combined as (
-    select 
-        u.user_id,
-        u.created_at,
-        u.user_type,
-        ug.is_international,
-        ug.country
-    from users u
-    left join user_geos ug 
-        on ug.user_id = u.user_id
-),
-
-school_years as (
+-- combined as (
+--     select 
+--         u.user_id,
+--         u.created_at,
+--         u.user_type,
+--         ug.is_international,
+--         ug.country
+--     from users u
+--     left join user_geos ug 
+--         on ug.user_id = u.user_id
+-- ),
+all_users as (
+    select *
+    from {{ref('dim_users')}}
+)
+, school_years as (
     select *
     from {{ ref('int_school_years') }}
 ),
@@ -45,12 +48,13 @@ final as (
     select
         c.user_type,
         c.country,
-        case when c.is_international = 1 then 'intl' else 'us' end  as us_intl,
+        c.us_intl,
+        --case when c.is_international = 1 then 'intl' else 'us' end  as us_intl,
         sy.school_year                                              as created_at_school_year,
         date_part(year, c.created_at)                               as created_at_year,
         date_part(month, c.created_at)                              as created_at_month,
         count(distinct c.user_id)                                   as num_accounts
-    from combined as c
+    from all_users as c
     left join school_years as sy 
         on c.created_at 
             between sy.started_at and sy.ended_at
