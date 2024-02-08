44a9922bfab59169965c53bc1d41adb3a77de96d
baker@code.org <baker@code.org>
2024-02-08 16:51:30 +0000

    added country to monthly accounts created

--- dbt/models/marts/metrics/fct_monthly_accounts_created.sql
+++ dbt/models/marts/metrics/fct_monthly_accounts_created.sql
@@ -18,7 +18,8 @@ users as (
 user_geos as (
     select 
         user_id,
-        is_international
+        is_international,
+        country
     from {{ref('stg_dashboard__user_geos')}}
     where user_id in (select user_id from users)
 ),
@@ -28,7 +29,8 @@ combined as (
         u.user_id,
         u.created_at,
         u.user_type,
-        ug.is_international
+        ug.is_international,
+        ug.country
     from users u
     left join user_geos ug 
         on ug.user_id = u.user_id
@@ -42,6 +44,7 @@ school_years as (
 final as (
     select
         c.user_type,
+        c.country,
         case when c.is_international = 1 then 'intl' else 'us' end  as us_intl,
         sy.school_year                                              as created_at_school_year,
         date_part(year, c.created_at)                               as created_at_year,
@@ -51,7 +54,7 @@ final as (
     left join school_years as sy 
         on c.created_at 
             between sy.started_at and sy.ended_at
-    {{ dbt_utils.group_by(5) }}
+    {{ dbt_utils.group_by(6) }}
 )
 
 select * 
