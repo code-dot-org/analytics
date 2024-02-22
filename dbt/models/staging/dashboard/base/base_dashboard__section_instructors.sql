08cd5fc817208d9c5e75f4560e05cf02a82afff0
jørdan <jas4235@outlook.com>
2024-02-22 10:36:59 -0500

    staging section_instructors data

---
+++ dbt/models/staging/dashboard/base/base_dashboard__section_instructors.sql
@@ -0,0 +1,20 @@
+with
+source as (
+    select *
+    from {{ source('dashboard', 'section_instructors') }}
+),
+
+renamed as (
+    select 
+        id as section_instructor_id,
+        instructor_id,
+        section_id,
+        invited_by_id,
+        status,
+        created_at,
+        updated_at,
+        deleted_at
+    from source)
+
+select *
+from renamed
