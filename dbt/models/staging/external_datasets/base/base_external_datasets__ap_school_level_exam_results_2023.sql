0b4d5a85e37835be6a9105a98a85ea43874c46e4
baker@code.org <baker@code.org>
2024-04-18 17:49:58 +0000

    commit after change, but not compiling yet

---
+++ dbt/models/staging/external_datasets/base/base_external_datasets__ap_school_level_exam_results_2023.sql
@@ -0,0 +1,13 @@
+/* 
+    This should be just a straight load of the data from source, and should be a 1:1 - CB dataset : dashboard table
+    TBD: do we want to continue to use dashboard.analysis for the COPY from S3, or do we want to make a space/schema for it in dev?
+
+    TODO: rename the source tables according to naming convention
+*/ 
+
+with ap_data AS (
+    select * 
+    from {{source('dashboard_analysis','stg_ap_exam_results_school_level_2023')}}
+)
+select * 
+from ap_data
