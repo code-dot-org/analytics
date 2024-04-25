ea025cef0494f01dedd48325c38f2fd4ccd79065
jordan.springer@code.org <jordan.springer@code.org>
2024-04-25 01:32:26 +0000

    stashing...

--- dbt/models/marts/metrics/fct_monthly_accounts_created.sql
+++ dbt/models/marts/metrics/fct_monthly_accounts_created.sql
@@ -2,10 +2,14 @@ with
 users as (
     select
         user_id,
-        created_at,
         user_type,
         us_intl,
-        country
+        country,
+        created_at, --timestamp
+        date_trunc('day',created_at)::date      as created_dt,
+        date_trunc('month',created_at)::date    as created_month,
+        date_trunc('year',created_at)::date     as created_year,
+        left(to_char(created_at, 'YYYY-MM'),7)  as created_year_month
     from {{ ref('dim_users') }}
     where current_sign_in_at is not null -- exclude dummy accounts
 ),
@@ -20,15 +24,17 @@ final as (
         u.user_type,
         u.us_intl,
         u.country,
-        sy.school_year                  as school_year,
-        date_part(year, u.created_at)   as created_year,
-        date_part(month, u.created_at)  as created_month,
-        count(distinct u.user_id)       as num_users_created
-    from users as u
-    join school_years as sy
-        on u.created_at
+        sy.school_year,
+        u.created_month,
+        u.created_year,
+        u.created_year_month,
+        count(distinct u.user_id) as num_users_created
+
+    from users          as u
+    join school_years   as sy
+        on u.created_dt
             between sy.started_at and sy.ended_at
-    {{ dbt_utils.group_by(6) }}
+    {{ dbt_utils.group_by(7) }}
 )
 
 select *
