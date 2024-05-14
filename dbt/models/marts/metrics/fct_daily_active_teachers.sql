35b4af03818bc68fdb21a9d9b98e3b632207e2a9
baker@code.org <baker@code.org>
2024-05-14 16:42:36 +0000

    fixed string concatenation

--- dbt/models/marts/metrics/fct_daily_active_teachers.sql
+++ dbt/models/marts/metrics/fct_daily_active_teachers.sql
@@ -6,7 +6,7 @@ select
     us_intl_merged us_intl,
     case when user_type_merged = 'teacher' then 'cdo teacher' 
          when user_type_merged = 'amp user' then 'anonymous' 
-         else 'uknown user type: ' | user_type_merged 
+         else 'unexpected user type ' || user_type_merged 
     end as user_type,
     known_cdo_user,
     count(distinct( user_id_merged)) num_teachers
