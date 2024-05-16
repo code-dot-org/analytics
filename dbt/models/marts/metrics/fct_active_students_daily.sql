1bd3eafe76b0175a0116214fcab8f133bea0fa19
baker@code.org <baker@code.org>
2024-05-16 12:42:09 +0000

    fixed tests on metrics fct tables to account for anon and student user types

--- dbt/models/marts/metrics/fct_active_students_daily.sql
+++ dbt/models/marts/metrics/fct_active_students_daily.sql
@@ -4,9 +4,10 @@ with actives as (
 )
 select
     activity_date as "date",
+    user_type_merged,
     country,
     us_intl,
     count(distinct(user_id)) num_actives
 from actives
-{{dbt_utils.group_by(3)}}
+{{dbt_utils.group_by(4)}}
 order by 1,2
--- dbt/models/marts/metrics/fct_active_students_monthly.sql
+++ dbt/models/marts/metrics/fct_active_students_monthly.sql
@@ -1,5 +1,5 @@
 /*
-    Number of 
+    Number of active students per month
 */
 with actives as (
     select *
@@ -7,9 +7,10 @@ with actives as (
 )
 select
     date_trunc('month', activity_date)::date as "month_year",
+    user_type_merged,
     country,
     us_intl,
     count(distinct(user_id)) num_actives
 from actives
-{{dbt_utils.group_by(3)}}
+{{dbt_utils.group_by(4)}}
 order by 1,2
