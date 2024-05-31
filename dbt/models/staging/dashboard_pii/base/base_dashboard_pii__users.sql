c03f6e18fc6d9747217a19b3197ca42ac0acb5e7
jordan.springer@code.org <jordan.springer@code.org>
2024-05-31 21:17:31 +0000

    cleaning up us_state values

--- dbt/models/staging/dashboard_pii/base/base_dashboard_pii__users.sql
+++ dbt/models/staging/dashboard_pii/base/base_dashboard_pii__users.sql
@@ -19,10 +19,11 @@ renamed as (
         updated_at,
         purged_at,
         deleted_at,
-        json_extract_path_text(
+        nullif(
+            json_extract_path_text(
             lower(properties), 
             'us_state',    
-             true)                  as us_state 
+             true),'')          as us_state 
     from source )
 
 select *
--- dbt/models/staging/dashboard_pii/stg_dashboard_pii__users.sql
+++ dbt/models/staging/dashboard_pii/stg_dashboard_pii__users.sql
@@ -7,23 +7,50 @@ users as (
 ),
 
 state_abbreviations as (
-    select 
-        lower(state_abbreviation) as us_state_abbr,
-        lower(state_name) as us_state_name
+    select distinct
+        lower(state_abbreviation)   as us_state_abbr,
+        lower(state_name)           as us_state_name,
+        1                           as is_us_state
     from {{ ref('seed_state_abbreviations') }}
 ),
+
+combined as (
+    select 
+        usr.* , 
+        case 
+        
+            when    sta.us_state_name is not null 
+                and sta.is_us_state = 1
+            then sta.us_state_abbr -- always return varchar(2) value 
+
+            when    sta.us_state_abbr is not null 
+                and sta.is_us_state = 1
+            then sta.us_state_abbr 
+            
+            else null end as us_state_abbr -- if not one of the US States, then null 
+
+    from users                      as usr
+    left join state_abbreviations   as sta 
+        on usr.us_state = 
+            coalesce(
+                sta.us_state_name,
+                sta.us_state_abbr ) -- if us_state matches a us state, return the abbreviate (varchar (2) )
+    ),
 
 renamed as (
     select
         user_id,
         user_type,
-        case when user_type = 'student' then user_id end as student_id,
-        case when user_type = 'teacher' then user_id end as teacher_id,
+        case when user_type = 'student' then user_id end            as student_id,
         
-        case when user_type = 'teacher' then email else null end as teacher_email, -- PII!
+        case when user_type = 'teacher' then user_id end            as teacher_id,
+        case when user_type = 'teacher' then email else null end    as teacher_email, -- PII!
+        
         birthday,
-        datediff(year, birthday, current_date) as age_years,
+        datediff(year, birthday, current_date)  as age_years,
         races,
+        us_state_abbr                           as us_state, -- entered originally by user
+
         case
             -- If races contains 'hispanic', return 'hispanic'
             when races like '%hispanic%' then 'hispanic'
@@ -43,10 +70,12 @@ renamed as (
             when races is null then 'not_collected'
             
             -- Default case: return the input value
-            else races -- Additional logic may be required here
-        end as race_group,
+            else races 
+            -- Additional logic may be required here
+        end                                     as race_group,
 
-        nullif(lower(gender), '') as gender,
+        nullif(lower(gender), '')               as gender,
+        
         case
             when gender = 'm' then 'm'
             when gender = 'f' then 'f'
@@ -60,20 +89,9 @@ renamed as (
                 then 'not_collected'
                 
             else 'unexpected value: ' || gender
-        end as gender_group,
-        
-        coalesce(
-            -- if the value is as desired, keep it
-            usr.us_state,
-            
-            -- if the value is a state name, refer to table for its abbreviation
-            sta.us_state_abbr)      as us_state
+        end as gender_group      
 
-    from users                      as usr 
-    left join state_abbreviations   as sta
-        on usr.us_state = sta.us_state_abbr -- eg. 'oh' = 'oh'
-        or usr.us_state = sta.us_state_name -- eg. 'ohio' = 'ohio'
-)
+    from combined )
 
 select *
 from renamed
