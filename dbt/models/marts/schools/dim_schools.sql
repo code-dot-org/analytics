e589600bf1191576ef6973d16d08062c08a306e6
jxrdan <jas4235@outlook.com>
2024-05-30 21:49:28 -0400

    Revert "updating alias for dim_schools.school_year"

--- dbt/models/marts/schools/dim_schools.sql
+++ dbt/models/marts/schools/dim_schools.sql
@@ -12,12 +12,7 @@ school_districts as (
 ),
 
 school_stats_by_years as (
-    select *, 
-        row_number() 
-            over(
-                partition by school_id 
-                order by school_year desc) as row_num
-
+    select *, row_number() over(partition by school_id order by school_year desc) as row_num
     from {{ ref('stg_dashboard__school_stats_by_years') }}
 ),
 
@@ -28,29 +23,24 @@ combined as (
         schools.city,
         schools.state,
         schools.zip,
-
-        school_stats_by_years.school_year   as most_recent_survey_school_year,
+        school_stats_by_years.school_year,
         school_stats_by_years.is_stage_el,
         school_stats_by_years.is_stage_mi,
         school_stats_by_years.is_stage_hi,
-        
         (
             (case when school_stats_by_years.is_stage_el = 1 then 'el' else '__' end ) ||
             (case when school_stats_by_years.is_stage_mi = 1 then 'mi' else '__' end ) ||
             (case when school_stats_by_years.is_stage_hi = 1 then 'hi' else '__' end ) 
         ) as school_level_simple,
-
         school_stats_by_years.is_rural,
         school_stats_by_years.is_title_i,
         school_stats_by_years.community_type,
-
         schools.school_category,
         schools.school_name,
         schools.school_type,
         case when total_frl_eligible_students / total_students::float > 0.5
             then 1 else 0 
         end as is_high_needs,
-
         schools.last_known_school_year_open,
 
         --school_districts
@@ -103,7 +93,6 @@ combined as (
         -- dates 
         min(schools.created_at) as school_created_at,
         max(schools.updated_at) as school_last_updated_at
-
     from schools
     left join school_stats_by_years
         on schools.school_id = school_stats_by_years.school_id
