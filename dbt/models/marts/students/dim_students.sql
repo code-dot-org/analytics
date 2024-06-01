c57ccf640f9a5b19a82333705c5364f7cce53c89
jxrdan <jordan@code.org>
2024-05-31 22:18:14 -0400

    Update dim_students.sql

--- dbt/models/marts/students/dim_students.sql
+++ dbt/models/marts/students/dim_students.sql
@@ -9,24 +9,15 @@ school_years as (
     select * from {{ref('int_school_years')}}
 ),
 
-section_mapping as (
-    select * 
-    from {{ref('int_section_mapping')}}
-),
-
 final as (
     select 
         students.*, 
-        sm.school_id,
         sy.school_year as created_at_school_year
     from students 
     left join school_years as sy 
         on students.created_at 
             between sy.started_at 
-                and sy.ended_at
-    left join section_mapping as sm 
-        on students.student_id = sm.student_id
-        and students.school_info_id = sm.school_info_id )
+                and sy.ended_at )
 
 select * 
 from final 
