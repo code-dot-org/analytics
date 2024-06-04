bee7bc65131127e4bb10199a52055c942c57c847
baker@code.org <baker@code.org>
2024-06-04 17:48:17 +0000

    fixed ap_ledgers -- source data was bad!

--- dbt/models/staging/external_datasets/base/base_external_datasets__ap_ledgers_2023.sql
+++ dbt/models/staging/external_datasets/base/base_external_datasets__ap_ledgers_2023.sql
@@ -2,21 +2,25 @@
     It's assumed that this table: analysis.stg_ap_ledgers_raw_2023 contains the load
     of any/all data files containing ledger data for 2023 (=2022-23 school year)
     see: ap_ledger_ingest code.
+
+    The raw data frequently has a duplicate or two slipped in there.  some lightweight de-duping is done here at the base.
+
 */ 
 
 with all_ledgers AS (
-    select * 
+    select
+        exam_year,
+        school_year,
+        exam,
+        ledger_group,
+        ai_code,
+        max(school_name) as school_name,
+        max(city) as city,
+        max(state) as state,
+        max(country) as country,
+        max(provider_syllabus) as provider_syllabus
     from {{source('external_datasets','stg_ap_ledgers_raw_2023')}}
+    {{dbt_utils.group_by(5)}}
 )
-select
-    exam_year,
-    school_year,
-    exam,
-    ledger_group,
-    ai_code,
-    school_name,
-    city,
-    state,
-    country,
-    provider_syllabus 
+select *
 from all_ledgers
