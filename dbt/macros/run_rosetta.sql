b31e6c623d35aa2f8c982c4772a6e5ffcf388517
jordan.springer@code.org <jordan.springer@code.org>
2024-06-29 04:56:47 +0000

    fixes

---
+++ dbt/macros/run_rosetta.sql
@@ -0,0 +1,10 @@
+{#
+   name: run_proc__rosetta
+   usage: dbt run-operation run_proc__rosetta
+   changelog:
+    - v1 (js) 2024-06-29
+#}
+
+{% macro run_rosetta() %}
+    CALL analysis.run_rosetta();
+{% endmacro %}
