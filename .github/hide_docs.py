8459dba86149f9f09e77715860c03f00e638608e
allison-code-dot-org <113540108+allison-code-dot-org@users.noreply.github.com>
2024-07-18 16:27:58 -0700

    hide redshift nodes also

--- .github/hide_docs.py
+++ .github/hide_docs.py
@@ -8,5 +8,9 @@ with open('dbt/target/manifest.json', 'r') as f:
 for macro in data['macros']:
   data['macros'][macro]['docs']['show'] = False
 
+for node in data['nodes']:
+  if 'model.redshift' in node:
+    data['nodes'][node]['docs']['show'] = False
+
 with open('dbt/target/manifest.json', 'w') as f:
   json.dump(data, f)
