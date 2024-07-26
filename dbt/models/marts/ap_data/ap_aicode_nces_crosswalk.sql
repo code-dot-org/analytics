33f205f37866a0982ae05c206204cd175898c192
allison@code.org <allison@code.org>
2024-07-26 23:28:05 +0000

    more formatting

--- dbt/models/marts/ap_data/ap_aicode_nces_crosswalk.sql
+++ dbt/models/marts/ap_data/ap_aicode_nces_crosswalk.sql
@@ -5,15 +5,17 @@
 with all_crosswalks as (
     select *
     from {{ ref('stg_external_datasets__ap_crosswalks') }}
-)
-, ranked_most_recent_record AS (
-    SELECT
-        *,
-        ROW_NUMBER() OVER (PARTITION BY ai_code ORDER BY exam_year DESC) AS rn
-    FROM
+),
+
+ranked_most_recent_record as (
+    select
+        *
+        , row_number() over (partition by ai_code order by exam_year desc)                  as rn
+    from
         all_crosswalks
 )
-SELECT
+
+select
     --rn,
     ai_code,
     nces_id,
@@ -21,7 +23,7 @@ SELECT
     city,
     state,
     zip,
-    exam_year as most_recent_update_year,
+    exam_year                                                                               as most_recent_update_year,
     source
 FROM
     ranked_most_recent_record
--- dbt/models/marts/ap_data/ap_ledgers.sql
+++ dbt/models/marts/ap_data/ap_ledgers.sql
@@ -3,11 +3,12 @@
 */
 with all_ledgers as (
     select 
-        l.*,
-        cw.nces_id
+        l.*
+        , cw.nces_id
     from {{ ref('stg_external_datasets__ap_ledgers') }} l
     left join {{ ref('ap_aicode_nces_crosswalk') }} cw on cw.ai_code = l.ai_code
 )
+
 select
     exam_year,
     school_year,
