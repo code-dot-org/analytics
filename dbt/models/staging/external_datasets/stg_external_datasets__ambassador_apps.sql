745a0123664b11207c35abb1b7bdd441e6bbd70a
allison@code.org <allison@code.org>
2024-07-30 19:23:16 +0000

    saving progress

--- dbt/models/staging/external_datasets/stg_external_datasets__ambassador_apps.sql
+++ dbt/models/staging/external_datasets/stg_external_datasets__ambassador_apps.sql
@@ -14,5 +14,18 @@ select
     , teacher_email_cdo
     , teacher_email_alt
     , state
-    , case when 
+    , replace(
+        replace(
+            replace(
+                replace(
+                    replace(
+                        replace(
+                            replace(
+                                courses_taught, 'CS Principles', 'csp'), 
+                        'AP® CSA', 'csa'), 
+                    'CS Discoveries', 'csd'), 
+                'AP Computer Science Principles', 'csp'), 
+            'AP Computer Science A', 'csa'), 
+        'AP A', 'csa'),
+    'AP csp', 'csp')
 from cs_ambassadors
