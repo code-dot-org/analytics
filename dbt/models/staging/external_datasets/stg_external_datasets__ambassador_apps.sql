0972b9eb45b66db81e6d7bf12f84b60344baed46
allison@code.org <allison@code.org>
2024-07-30 18:00:59 +0000

    saving progress

--- dbt/models/staging/external_datasets/stg_external_datasets__ambassador_apps.sql
+++ dbt/models/staging/external_datasets/stg_external_datasets__ambassador_apps.sql
@@ -5,5 +5,14 @@ cs_ambassadors as (
 )
 
 select 
-    *
+    created_dt
+    , teacher_email
+    , cdo_teacher
+    , teacher_first_name   
+    , teacher_last_name
+    , teacher_email_school
+    , teacher_email_cdo
+    , teacher_email_alt
+    , state
+    , case when 
 from cs_ambassadors
