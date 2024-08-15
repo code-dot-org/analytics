d5fa55a7b5e7627290dac764844daa01b97b15e6
allison@code.org <allison@code.org>
2024-08-15 19:44:27 +0000

    deleting activity_month

--- dbt/models/marts/students/dim_student_script_level_activity.sql
+++ dbt/models/marts/students/dim_student_script_level_activity.sql
@@ -54,7 +54,7 @@ select
     , date_trunc('day', ul.created_at)                              as activity_date
 
     -- time variables 
-    , date_trunc ('month', ul.created_at)                           as activity_month  
+    --, date_trunc ('month', ul.created_at)                           as activity_month  
     , case 
         when extract ('month' from ul.created_at) in (7,8,9) then 'Q1'
         when extract ('month' from ul.created_at) in (10,11,12) then 'Q2'
