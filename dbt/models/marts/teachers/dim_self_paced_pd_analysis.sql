d9c2d8cc48c474c0ff86feb82a061900294b781f
allison-code-dot-org <113540108+allison-code-dot-org@users.noreply.github.com>
2024-08-16 13:55:24 -0700

    Update dbt/models/marts/teachers/dim_self_paced_pd_analysis.sql
    Co-authored-by: Natalia Zacipa Mejia <98911841+nataliazm99@users.noreply.github.com>

--- dbt/models/marts/teachers/dim_self_paced_pd_analysis.sql
+++ dbt/models/marts/teachers/dim_self_paced_pd_analysis.sql
@@ -83,7 +83,7 @@ active_teachers as (
                 rows between unbounded preceding and unbounded following)               as max_script_id
         , last_value (script_name) 
 	        over (
-                partition by teacher_id, course_name
+                partition by teacher_id, course_name_true
 			    order by level_created_at 
                 rows between unbounded preceding and unbounded following)               as max_script_name
         , last_value (level_id) 
