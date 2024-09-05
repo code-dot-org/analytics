e96d51749b98e6811326d12accb5eb335f0050af
jxrdan <jordan@code.org>
2024-09-05 10:55:05 -0400

    Update stg_dashboard__ai_tutor_interaction_feedbacks.sql

--- dbt/models/staging/dashboard/stg_dashboard__ai_tutor_interaction_feedbacks.sql
+++ dbt/models/staging/dashboard/stg_dashboard__ai_tutor_interaction_feedbacks.sql
@@ -1,19 +1,23 @@
 with 
 
-base as ( 
+feedbacks as ( 
     select * 
     from {{ ref('base_dashboard__ai_tutor_interaction_feedbacks') }}
-)
+),
 
-select 
-    id                                          as feedback_id
-    , 'ai_tutor'                                as feature_name
-    , user_id 
-    , case 
-        when thumbs_up = 1 then 1 
-        when thumbs_down = 1 then 0 
-        else null 
-    end                                         as thumbs_up
-    , details 
-    , created_at
-from base 
+renamed as (
+    select 
+        id                                          as feedback_id
+        , 'ai_tutor'                                as feature_name
+        , user_id 
+        , case 
+            when thumbs_up = 1 then 1 
+            when thumbs_down = 1 then 0 
+            else null 
+        end                                         as thumbs_up
+        , details 
+        , created_at
+    from feedbacks )
+
+select * 
+from renamed 
