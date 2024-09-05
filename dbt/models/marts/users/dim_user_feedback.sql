932bfe0c9199f57265820401eeb563f35ddf2bf3
jxrdan <jordan@code.org>
2024-09-05 10:56:00 -0400

    Update dim_user_feedback.sql

--- dbt/models/marts/users/dim_user_feedback.sql
+++ dbt/models/marts/users/dim_user_feedback.sql
@@ -8,16 +8,21 @@ ai_feedback as (
 school_years as (
     select * 
     from {{ ref('int_school_years') }}
-)
+),
 
-select 
-    ai_feedback.feedback_id 
-    , ai_feedback.feature_name
-    , ai_feedback.user_id
-    , ai_feedback.thumbs_up 
-    , ai_feedback.created_at
-    , school_years.school_year 
-    , ai_feedback.details
-from ai_feedback
-join school_years 
-    on ai_feedback.created_at between school_years.started_at and school_years.ended_at
+final as (
+    select 
+        ai_feedback.feedback_id 
+        , ai_feedback.feature_name
+        , ai_feedback.user_id
+        , ai_feedback.thumbs_up 
+        , ai_feedback.created_at
+        , school_years.school_year 
+        , ai_feedback.details
+    from ai_feedback
+    join school_years 
+        on ai_feedback.created_at 
+        between school_years.started_at 
+            and school_years.ended_at )
+select * 
+from final 
