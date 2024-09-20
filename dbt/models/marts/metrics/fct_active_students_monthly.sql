352e90c83901f4f9a220226fee6b03892c47bcc6
Jordan Springer <jordan@code.org>
2024-09-20 14:08:35 +0000

    moar

--- dbt/models/marts/metrics/fct_active_students_monthly.sql
+++ dbt/models/marts/metrics/fct_active_students_monthly.sql
@@ -1,43 +1,54 @@
 -- model: fct_active_students_monthly
 -- version 2.0 (js; 2024-09-17)
 
-with cutoff_date as (select '2020-06-30'::date as cutoff_date),
-
+with 
 user_levels as (
     select  
-        created_date, 
-        user_id,
-        1 as is_active
+        created_date as activity_date
+        user_id as student_id,
+        1 as is_active,
+        1 as has_user_level_activity
     from {{ ref('dim_user_levels') }}
-    where created_date > (select cutoff_date from cutoff_date)
+    where created_date > {{ get_cutoff_date() }}
         and total_attempts > 0 
 ), 
 
 sign_ins as (
     select 
-        sign_in_date,
-        user_id,
-        1 as is_active
+        sign_in_date as activity_date,
+        user_id as student_id,
+        1 as is_active, 
+        1 as has_sign_in_activity
     from {{ ref('dim_user_sign_ins') }}
-    where sign_in_date > (select cutoff_date from cutoff_date)
+    where sign_in_date > {{ get_cutoff_date() }}
         and num_sign_ins > 0
 ), 
 
 projects as (
     select 
-        project_created_at::date 
-        user_id,
-        known_cdo_user, -- includes anonymous users (0=anon, 1=known)
-        1 as is_active
+        project_created_at::date as activity_date,
+        user_id as student_id,
+        -- known_cdo_user, -- includes anonymous users (0=anon, 1=known)
+        1 as is_active,
+        1 as has_project_activity
 
     from {{ ref('dim_student_projects') }}
-    where project_created_at > (select cutoff_date from cutoff_date)
-    
-    {{ dbt_utils.group_by(6) }}
-    having num_rows > 0 
+    where project_created_at > {{ get_cutoff_date() }}
 ), -- select * from projects 
 
 combined as (
+    select
+    activity_date,
+    count(distinct 
+    case when has_sign_in_activity = 1
+    and coalesce(has_user_level_activity,has_project_activity)=1
+    then student_id end) as num_active_students
+    from ... 
+
+
+
+
+    /*
     select 'User Levels'                    as activity_type,
         date_trunc('month',activity_date)   as activity_month,
         country, 
