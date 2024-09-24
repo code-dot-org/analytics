2969057fa770a3c508409b2fcec7078606968928
allison@code.org <allison@code.org>
2024-09-24 20:01:06 +0000

    changes to dim_cs_access

--- dbt/models/marts/access_report/dim_cs_access.sql
+++ dbt/models/marts/access_report/dim_cs_access.sql
@@ -1,8 +1,19 @@
 with 
 
 review_table as (
-    select * 
+    select 
+        access_report_year
+        , nces_school_id
+        , state
+        , school_name
+        , grade_levels
+        , school_type 
+        , case 
+            when teaches_cs_final in ('HY', 'Y') then 1 
+            else 0 
+        end                                                     as teaches_cs
     from {{ ref('stg_external_datasets__access_report_review_table') }}
+    
 )
 
 select * 
--- dbt/models/staging/external_datasets/stg_external_datasets__access_report_review_table.sql
+++ dbt/models/staging/external_datasets/stg_external_datasets__access_report_review_table.sql
@@ -21,3 +21,7 @@ with all_data as (
 )
 select *
 from all_data
+where state not in ('AS', 'GU', 'MP', 'PR', 'VI') -- exclude territories
+and teaches_cs_final not in ('unknown', 'E', 'flag') -- exclude schools with unknown CS status
+and grade_levels != '______' -- exclude schools with no grade levels
+and school_type in ('public', 'charter')
