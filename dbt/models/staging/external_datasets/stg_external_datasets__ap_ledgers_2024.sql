e4e7fde3c89150dd911b9e7a32ef536014e384a2
Cory Kind <cory.kind@code.org>
2024-10-10 13:27:39 +0000

    rearranging cte's based on Allison's feedback

--- dbt/models/staging/external_datasets/stg_external_datasets__ap_ledgers_2024.sql
+++ dbt/models/staging/external_datasets/stg_external_datasets__ap_ledgers_2024.sql
@@ -11,34 +11,38 @@
     We should use address + zip from NCES data instead
 */ 
 
-with all_ledgers AS (
+with new_ledgers AS (
+    select * from {{ ref('base_external_datasets__ap_ledgers_2024') }}
+), 
+
+transformed_ledger AS (
     select
-        cast(exam_year as varchar) as exam_year,
-        school_year,
-        case when exam='Computer Science Principles' then 'csp'
-            when exam= 'Computer Science A' then 'csa'
-            end as exam,
-        cast(ai_code as varchar) as ai_code,
-        school_name as school_name,
-        city as city,
-        state as state,
-        case when state in ('AL','AK','AZ','AR','CA','CO','CT','DE','DC','FL',
-                            'GA','HI','ID','IL','IN','IA','KS','KY','LA','ME',
-                            'MD','MA','MI','MN','MS','MO','MT','NE','NV','NH',
-                            'NJ','NM','NY','NC','ND','OH','OK','OR','PA','PR',
-                            'RI','SC','SD','TN','TX','UT','VT','VA','WA','WV',
-                            'WI','WY') then 'US' 
-                    else NULL 
-                    end as country,
-        case when country = 'US' then zip
-                    else NULL
-                    end as zip,
-        case when provider_syllabus = '' then Null 
-            when provider_syllabus = 'Code.org + CMU CS Academy Sample Syllabus' then 'Code.org + CMU CS Academy'
-            when provider_syllabus = 'Code.org Sample Syllabus' then 'Code.org' 
-            when provider_syllabus = 'Code.org Sample Syllabus (For Pilot Teachers Only)' then 'Code.org' 
-            end as provider_syllabus
-    from {{ ref('base_external_datasets__ap_ledgers_2024') }}
+    cast(exam_year as varchar) as exam_year,
+    school_year,
+    case when exam='Computer Science Principles' then 'csp'
+        when exam= 'Computer Science A' then 'csa'
+        end as exam,
+    cast(ai_code as varchar) as ai_code,
+    school_name as school_name,
+    city as city,
+    state as state,
+    case when state in ('AL','AK','AZ','AR','CA','CO','CT','DE','DC','FL',
+                        'GA','HI','ID','IL','IN','IA','KS','KY','LA','ME',
+                        'MD','MA','MI','MN','MS','MO','MT','NE','NV','NH',
+                        'NJ','NM','NY','NC','ND','OH','OK','OR','PA','PR',
+                        'RI','SC','SD','TN','TX','UT','VT','VA','WA','WV',
+                        'WI','WY') then 'US' 
+        else NULL 
+        end as country,
+    case when country = 'US' then zip
+        else NULL
+        end as zip,
+    case when provider_syllabus = '' then Null 
+        when provider_syllabus = 'Code.org + CMU CS Academy Sample Syllabus' then 'Code.org + CMU CS Academy'
+        when provider_syllabus = 'Code.org Sample Syllabus' then 'Code.org' 
+        when provider_syllabus = 'Code.org Sample Syllabus (For Pilot Teachers Only)' then 'Code.org' 
+        end as provider_syllabus
+    from new_ledgers
 ),
 
 national_group AS (
@@ -52,7 +56,7 @@ national_group AS (
     state,
     country,
     NULL as provider_syllabus
-    from all_ledgers
+    from transformed_ledger
 ),
 
 cdo_audit_group AS (
@@ -66,7 +70,7 @@ cdo_audit_group AS (
     state,
     country,
     provider_syllabus
-    from all_ledgers
+    from transformed_ledger
     where provider_syllabus in ('Code.org', 'Code.org + CMU CS Academy')
 )
 
