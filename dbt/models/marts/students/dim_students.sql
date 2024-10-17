7d2a5273acfeefd04350a18c56dc5c37b9923ed3
jørdan <jas4235@outlook.com>
2024-10-17 11:15:38 -0400

    adding fields to dim_students model

--- dbt/models/marts/students/dim_students.sql
+++ dbt/models/marts/students/dim_students.sql
@@ -31,9 +31,12 @@ final as (
         sa.school_id,
         sy.school_year      as created_at_school_year, 
         students.is_urg,
-        students.gender,
-        students.locale,
+        students.gender_group,
+        students.race_group,
         students.birthday,
+        students.locale,
+        students.country,
+        students.us_intl,
 
         -- aggs 
         students.sign_in_count,
