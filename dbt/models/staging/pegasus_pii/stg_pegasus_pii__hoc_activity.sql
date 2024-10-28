5ee66fc921748d2af04aee66376c41679151ef2f
Cory Kind <cory.kind@code.org>
2024-10-28 20:01:54 +0000

    committing seed file + lookup logic

--- dbt/models/staging/pegasus_pii/stg_pegasus_pii__hoc_activity.sql
+++ dbt/models/staging/pegasus_pii/stg_pegasus_pii__hoc_activity.sql
@@ -1,40 +1,44 @@
-{{
+/*{{
     config(
         materialized='incremental',
         unique_key='hoc_start_id'
     )
-}}
+}}*/
 
-with
-hoc_starts as (
+with internal_tutorial_list as (
+    select * from {{ref('seed_hoc_internal_tutorials')}}
+)
+
+, hoc_starts as (
+    select * from {{ ref("base_pegasus_pii__hoc_activity") }}
+)
+
+, combined as (
+    select * from hoc_starts
+    left join internal_tutorial_list on hoc_starts.tutorial = internal_tutorial_list.tutorial_codes
+)
+
+, final as (
     select
         hoc_start_id,
         referer,
         company,
         tutorial,
         coalesce(started_at, pixel_started_at, pixel_finished_at)   as started_at,
-        case 
-            when 
-                coalesce(
-                    pixel_started_at
-                    , pixel_finished_at
-                ) is not null 
-            then 1 
-            else 0 
-        end                                                          as is_third_party,
+        case
+            when combined.is_internal is null then 1
+            else 0
+        end as is_third_party,
         country_code,
         state_code,
         city,
         country,
         state
-    from {{ ref("base_pegasus_pii__hoc_activity") }}
-    {% if is_incremental() %}
-
-    where coalesce(started_at, pixel_started_at, pixel_finished_at) > (select max(started_at) from {{ this }} )
-    
-    {% endif %}
+    from combined
+   /* {% if is_incremental() %}
+        where coalesce(started_at, pixel_started_at, pixel_finished_at) > (select max(started_at) from {{ this }} )
+    {% endif %}*/
 )
 
 select *
-from hoc_starts
-
+from final
