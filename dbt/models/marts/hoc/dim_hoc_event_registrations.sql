ee798703916116af54b2b042d5acb8f8d56aacb2
jørdan <jas4235@outlook.com>
2024-11-06 16:07:04 -0500

    moar

--- dbt/models/marts/hoc/dim_hoc_event_registrations.sql
+++ dbt/models/marts/hoc/dim_hoc_event_registrations.sql
@@ -1,21 +1,68 @@
-with forms as (
+with 
+forms as (
     select * 
     from {{ ref('dim_forms') }}
-)
+),
 
-select 
-    form_id
-    , form_kind
-    , email 
-    , hoc_year                                as cal_year
-    , school_year
-    , registered_at
-    , event_type
-    , email_pref
-    , special_event_flag
-    , review
-    , city
-    , state
-    , country
-from forms 
-where form_category = 'hoc'
+hoc_form_registrations as (
+    select 
+        form_id
+        , form_kind
+        , email 
+        , hoc_year                                as cal_year
+        , school_year
+        , registered_at
+        , trunc(registered_at)                    as registered_dt
+        , event_type
+        , email_pref
+        , special_event_flag
+        , review
+        , city
+        , state
+        , country
+    from forms 
+    where form_category = 'hoc'
+),
+
+hoc_prospects as (
+    select *
+    from {{ ref('stg_external_datasets__pardot_prospects') }}
+),
+
+combined as (
+    select 
+        form_id as registration_id,
+        {# email, #}
+        cal_year,
+        school_year,
+        registered_dt,
+        city,
+        state,
+        country 
+    from hoc_form_registrations
+    union all 
+    select 
+        prospect_id,
+        {# email, #}
+        cal_year,
+        school_year,
+        registered_dt,
+        city,
+        state,
+        country 
+    from hoc_prospects
+),
+
+final as (
+    select 
+        cal_year,
+        school_year,
+        registered_dt,
+        city,
+        state,
+        country ,
+        count(distinct registration_id) as num_registrations
+    from combined 
+    {{ dbt_utils.group_by(6) }} )
+
+select * from final order by registered_dt desc 
