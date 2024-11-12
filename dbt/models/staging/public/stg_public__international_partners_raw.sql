7c9caf6afc7a565e1962a5be7e12193b78f2e3da
Jordan Springer <jordan@code.org>
2024-11-12 17:28:44 +0000

    adding tests to stg_public models

--- dbt/models/staging/public/stg_public__international_partners_raw.sql
+++ dbt/models/staging/public/stg_public__international_partners_raw.sql
@@ -1,7 +1,5 @@
 with international_partners_raw as (
     select * 
-    from {{ ref ('base_public__international_partners_raw') }}
-)
+    from {{ ref ('base_public__international_partners_raw') }} )
 
-select * 
-from international_partners_raw
+select * from international_partners_raw
