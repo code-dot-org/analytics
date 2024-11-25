c08324a081b838f9586f97d454e0661451915e77
Jordan Springer <jordan@code.org>
2024-11-25 19:11:17 +0000

    stashing

--- dbt/models/marts/metrics/fct_monthly_active_students.sql
+++ dbt/models/marts/metrics/fct_monthly_active_students.sql
@@ -11,15 +11,42 @@ with active_students as (
     from {{ ref('dim_active_students') }}
 ),
 
-final as (
+school_years as (
+    select * 
+    from {{ref('int_school_years') }}
+),
+
+combined as (
     select 
-        date_trunc('month',activity_date) as activity_month,
+        sy.school_year,
+        date_trunc('month', activity_date) as activity_month,
         us_intl,
         country,
         count(distinct student_id) as num_active_students
-    from active_students
-    {{ dbt_utils.group_by(3) }} )
+    from active_students 
+    join school_years as sy 
+        on active_students.activity_date 
+            between sy.started_at
+                and sy.ended_at 
+    {{ dbt_utils.group_by(4) }}
+),
+
+final as (
+    select 
+        school_year,
+        activity_month,
+        us_intl,
+        country,
+        num_active_students,
+        sum(num_active_students) over(
+            partition by 
+                school_year,
+                us_intl,
+                country
+            order by activity_month
+            rows between unbounded preceding and current row
+        ) as num_active_students_ytd 
+    from combined )
     
 select * 
 from final 
-order by activity_month desc 
