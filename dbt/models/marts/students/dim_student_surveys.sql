0fa8e64666b9156f7c706abbd084a36e0d29274c
Jordan Springer <jordan@code.org>
2024-11-27 21:04:48 +0000

    saving progress...

---
+++ dbt/models/marts/students/dim_student_surveys.sql
@@ -0,0 +1,88 @@
+-- model: dim_student_surveys 
+
+with 
+contained_levels as (
+    select * 
+    from {{ ref('stg_dashboard__contained_levels') }}
+),
+
+levels as (
+    select * 
+    from {{ ref('stg_dashboard__levels') }}
+),
+
+script_levels as (
+    select *
+    from {{ ref('stg_dashboard__script_levels') }}
+),
+
+levels_script_levels as (
+    select * 
+    from {{ ref('stg_dashboard__levels_script_levels') }}
+),
+
+scripts as (
+    select * 
+    from {{ ref('stg_dashboard__scripts') }}   
+),
+
+course_structure as (
+    select * 
+    from {{ ref('dim_course_structure') }}
+    where course_name in ('csa','csd','csp')
+),
+
+combined as (
+    select 
+        lower(gl.level_name)            as survey_name,
+        case when survey_name like '%pre%' 
+              and survey_name not like '%preview%'  then 'pre'
+             when survey_name like '%post%'         then 'post'
+             when survey_name like '%pulse%'        then 'pulse'
+             when survey_name like '%end of unit%'  then 'end of unit'
+             else null end        as survey_type,
+
+        lower(cl.level_name)            as question_name,
+        lower(col.contained_level_type) as question_type,
+        lower(col.contained_level_text) as question_text,
+        col.contained_level_position    as question_position,
+        sc.script_name,
+        cs.course_name,
+        cs.unit, 
+        cs.content_area,
+        cs.topic_tags,
+        cs.version_year
+
+    from contained_levels   as col 
+    join levels             as gl -- group levels 
+    on col.level_group_level_id = gl.level_id
+
+    join levels             as cl -- contained levels 
+    on col.contained_level_id = cl.level_id
+    
+    join levels_script_levels   as lsl 
+    on gl.level_id = lsl.level_id
+    
+    join script_levels          as sl 
+    on lsl.script_level_id = sl.script_level_id 
+
+    join scripts as sc 
+    on sl.script_id = sc.script_id 
+
+    join course_structure as cs 
+    on sc.script_id = cs.script_id 
+    and gl.level_id = cs.level_id 
+
+    where gl.level_name like '%survey%' )
+
+select survey_type, count(distinct survey_name) as num_surveys
+from combined 
+group by 1 
+order by 2 desc 
+
+-- analysis.user_surveys
+-- Pulse: 114
+-- Pre: 23
+-- Post: 29 
+-- End of Unit: 78
+-- NULL: 6
