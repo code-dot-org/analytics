ea7c69b950ce0a89396e0c1f123528f490adbd73
Jordan Springer <jordan@code.org>
2024-11-27 18:46:26 +0000

    resolving final aggregation

--- dbt/models/marts/metrics/fct_monthly_active_students.sql
+++ dbt/models/marts/metrics/fct_monthly_active_students.sql
@@ -1,9 +1,9 @@
--- Step 0: Stage data 
+-- Step 0: Stage data
 with 
 active_students as (
     select 
         *, 
-        date_trunc('month',activity_date) as activity_month 
+        date_trunc('month', activity_date) as activity_month 
     from {{ ref('dim_active_students') }}
 ),
 
@@ -23,10 +23,9 @@ combined as (
         acs.us_intl,
         acs.country,
         acs.student_id
-        
-    from active_students    as acs 
+    from active_students as acs 
     left join first_active_month as fam 
-         on acs.student_id  = fam.student_id 
+         on acs.student_id = fam.student_id 
         and acs.school_year = fam.school_year
         and acs.activity_month >= fam.first_activity_month
 ),
@@ -39,15 +38,24 @@ final as (
         com.country,
         count(distinct com.student_id) as num_active_students,
         count(distinct case 
-            when com.activity_month = fam.first_activity_month 
-            then fam.student_id end)  as num_active_students_ytd
-    
-    from combined                   as com
-    left join first_active_month    as fam 
+            when com.activity_month = fam.first_activity_month
+            then com.student_id end) as num_new_students
+    from combined as com
+    left join first_active_month as fam 
         on com.student_id = fam.student_id 
         and com.school_year = fam.school_year
-    
-    {{ dbt_utils.group_by(4) }} 
+    group by com.school_year, com.activity_month, com.us_intl, com.country
+),
+
+rolling_final_prep as (
+    select 
+        school_year,
+        activity_month,
+        us_intl,
+        country,
+        num_active_students,
+        num_new_students
+    from final
 ),
 
 rolling_final as (
@@ -57,17 +65,18 @@ rolling_final as (
         us_intl,
         country,
         num_active_students,
-        sum(num_active_students_ytd) over(
+        sum(num_new_students) over (
             partition by 
-                school_year,
-                us_intl,
-                country 
+                school_year, 
+                us_intl, 
+                country
             order by activity_month
-            rows between unbounded preceding 
-                     and current row) as num_active_students_ytd 
-    from final 
-    {{ dbt_utils.group_by(5) }} )
+            rows between unbounded preceding and current row
+        ) as num_active_students_ytd
+    from rolling_final_prep )
 
 select * 
 from rolling_final
-order by activity_month desc 
+order by 
+    school_year, 
+    activity_month desc
