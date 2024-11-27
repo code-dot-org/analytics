f59ff401eeaf5f0ad76237e6af364ed79d00a07c
Cory Kind <cory.kind@code.org>
2024-11-27 21:09:36 +0000

    adding seed files + wip logic on adding the standard name

--- dbt/models/staging/dashboard/stg_dashboard__user_geos.sql
+++ dbt/models/staging/dashboard/stg_dashboard__user_geos.sql
@@ -10,32 +10,13 @@ user_geos as (
              when country <> 'united states' then 'intl'
              else null 
         end as us_intl,
-        case when country = 'åland' then 'åland islands'
-             when country = 'brunei' then 'brunei darussalam'
-             when country = 'cape verde' then 'cabo verde'
-             when country = 'cocos [keeling] islands' then 'cocos (keeling) islands'
-             when country = 'czech republic' then 'czechia'
-             when country = 'swaziland' then 'eswatini'
-             when country = 'federated states of micronesia' then 'micronesia, federated states of'
-             when country = 'ivory coast' then 'côte d''ivoire'
-             when country = 'hashemite kingdom of jordan' then 'jordan'
-             when country = 'republic of lithuania' then 'lithuania'
-             when country = 'republic of moldova' then 'moldova'
-             when country = 'myanmar [burma]' then 'myanmar'
-             when country = 'the netherlands' then 'netherlands'
-             when country = 'macedonia' then 'north macedonia'
-             when country = 'reserved' then null
-             when country = 'st kitts and nevis' then 'saint kitts and nevis'
-             when country = 'st vincent and grenadines' then 'saint vincent and the grenadines'
-             when country = 'slovak republic' then 'slovakia'
-             when country = 'republic of korea' then 'south korea'
-             when country = 'democratic republic of timor-leste' 
-                or country = 'east timor' then 'timor-leste'
-            when country = 'turkey' then 'türkiye'
-            else country
-        end as country_standardized
     from {{ ref('base_dashboard__user_geos') }}
 ),
+
+country_name_manual_changes as (
+    select *
+    from {{ref('seed_country_standardizations')}}
+)
 
 final as (
     select
@@ -46,7 +27,7 @@ final as (
         lower(city) as city,
         lower(state) as state_name,
         postal_code,
-        lower(country_standardized) as country,
+        ifnull(cnmc.iso_country, user_geos.country),
         is_international,
         us_intl,
         
@@ -55,6 +36,8 @@ final as (
         updated_at,
         indexed_at
     from user_geos
+    join country_name_manual_changes cnmc
+        on user_geos.country = cnmc.country_user_goes
     where row_number = 1
 )
 
