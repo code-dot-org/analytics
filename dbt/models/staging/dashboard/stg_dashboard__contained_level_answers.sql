500f5a895c64a91d431ca55d2aa6a3955c2840ee
Jordan Springer <jordan@code.org>
2024-12-20 23:56:32 +0000

    removind dev files, update contained_levels_answers

--- dbt/models/staging/dashboard/stg_dashboard__contained_level_answers.sql
+++ dbt/models/staging/dashboard/stg_dashboard__contained_level_answers.sql
@@ -3,11 +3,9 @@ contained_level_answers as (
     select 
         level_id,
         answer_number,
-        lower(answer_text) as answer_text,
+        answer_text,
         is_correct
     from {{ ref('base_dashboard__contained_level_answers') }} )
 
-select * 
+select *
 from contained_level_answers
-
- 
