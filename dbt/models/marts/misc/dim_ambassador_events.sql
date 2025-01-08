f702894d7431106470bd708848c358d1d62f24c1
allison@code.org <allison@code.org>
2025-01-08 19:22:13 +0000

    added docs

--- dbt/models/marts/misc/dim_ambassador_events.sql
+++ dbt/models/marts/misc/dim_ambassador_events.sql
@@ -6,6 +6,7 @@ amb_section_metrics as (
         code_studio_name,
         email,
         school_year,
+        min(section_created_dt) as section_created_dt,
         count(distinct section_id) as num_sections,
         count(distinct student_id) as num_students
     from {{ ref('dim_ambassador_activity') }}
@@ -25,45 +26,17 @@ section_course_list as (
 event_impact_survey as ( 
     select *
     from (select 
-            user_id,
-            code_studio_name,
-            email,
-            pivot(response_text for item_name in ('event_date'))
-        -- case 
-        --     when item_name = 'event_date' then response_text
-        --     else null 
-        -- end as event_date,
-        
-        -- case 
-        --     when item_name = 'event_type' then response_value
-        --     else null 
-        -- end as event_type,
-        
-        -- case 
-        --     when item_name = 'pre_enrollment' then response_text
-        --     else null 
-        -- end as pre_enrollment,
-        
-        -- case 
-        --     when item_name = 'post_enrollment' then response_text
-        --     else null 
-        -- end as post_enrollment,
-
-        -- case 
-        --     when item_name = 'total_participants' then response_text
-        --     else null 
-        -- end as total_participants
-    from {{ ref('dim_foorms') }}
-    where form_name = 'surveys/teachers/cs_ambassador_event'
-),
-
-event_impact_coalesce as (
-    select 
-        user_id,
-        code_studio_name,
-        email,
-    from event_impact_survey
-
+            foorm_submission_id, user_id, school_year, item_name, response_text
+            from {{ ref('dim_foorms') }}
+            where form_name = 'surveys/teachers/cs_ambassador_event')
+    pivot(max(response_text) for item_name in (
+        'event_date', 
+        'event_type',
+        'total_participants', 
+        'num_pre_enrollment',
+        'num_post_enrollment'
+        )
+    )
 )
 
 select 
@@ -71,11 +44,27 @@ select
     asm.code_studio_name,
     asm.email,
     asm.school_year,
-    case when eis.event_type
+    case 
+        when eis.event_type = 'experience cs (in code studio)' then 'experience_cs'
+        when eis.event_type = 'connect with cs (not in code studio)' then 'connect with cs'
+        else 
+            case 
+                when asm.num_sections > 0 then 'experience_cs'
+                else eis.event_type
+            end
+    end as event_type,
+    coalesce(to_timestamp(eis.event_date, 'YYYY-MM-DD HH24:MI:SS'), date_trunc('day', asm.section_created_dt)) as event_date,
+    eis.total_participants as survey_total_participants,
+    eis.num_pre_enrollment as survey_num_pre_enrollment,
+    eis.num_post_enrollment as survey_num_post_enrollment,
+    case 
+        when eis.num_pre_enrollment is not null and eis.num_post_enrollment is not null
+        then 1 
+        else 0 
+    end as impact_eval_flag,
     asm.num_sections,
-    asm.num_students,
-    scl.courses_touched,
-
+    asm.num_students as num_students_in_section,
+    scl.courses_touched as section_courses_touched
 
 from amb_section_metrics                    as asm 
 
