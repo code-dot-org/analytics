675a9ed3289e4bfb6ac4e808aa7c2c76e1c678fb
Cory Kind <cory.kind@code.org>
2025-02-10 21:54:40 +0000

    reverting changes to stg_public_international_partners_raw since this staging table isn't used

--- dbt/models/staging/public/stg_public__international_partners_raw.sql
+++ dbt/models/staging/public/stg_public__international_partners_raw.sql
@@ -1,11 +1,5 @@
 with international_partners_raw as (
     select *
-        {{country_normalization(diplay_name)}} as country,
-        display_name as country
-        lower(partner) as partner,
-        lower(workshop_organizers) as workshop_organizers,
-        lower(partner_type) as partner_type,
-        partner_id
     from {{ ref ('base_public__international_partners_raw') }}
 )
 
